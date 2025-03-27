506f4da3acad1750274c2fcb81083a234f24c00c
Aaron Lemus <aalberto01@unitec.edu>
2025-03-26 18:20:54 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace examen2_aaron_lemus_61421189.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
